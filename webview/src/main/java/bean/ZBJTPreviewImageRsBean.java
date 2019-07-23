9e58decb6b79258becbfca3e6bd3a01567d90ab2
李欣科 <lixink@8531.cn>
2019-07-23 19:00:01 +0800

    修复图片预览返回数据格式

--- webview/src/main/java/bean/ZBJTPreviewImageRsBean.java
+++ webview/src/main/java/bean/ZBJTPreviewImageRsBean.java
@@ -1,9 +1,16 @@
 package bean;
 
 import java.io.Serializable;
+import java.util.Map;
 
 public class ZBJTPreviewImageRsBean implements Serializable {
     private static final long serialVersionUID = 4708881291377835802L;
-    public String hasPreviewed;
-    public String hasSaved;
+    public int code;
+    public DataBean data;
+
+    public static class DataBean implements Serializable {
+        private static final long serialVersionUID = -1762435805853807368L;
+        public Map<Integer,String> hasPreviewed;
+        public Map<Integer,String> hasSaved;
+    }
 }
